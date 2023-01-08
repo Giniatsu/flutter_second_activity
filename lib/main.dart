604993edac125b32f18b907a0ab9736d16c94c73
Giniatsu <aragarcia@addu.edu.ph>
2023-01-08 18:48:41 +0800

    Nearing end of second activity

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() => runApp(
   MaterialApp(
@@ -21,9 +22,10 @@ class HomeScreen extends StatelessWidget {
         width: MediaQuery.of(context).size.width,
         child: Center(
           child: Card(
+            color: Color.fromARGB(255, 41, 29, 90),
             elevation: 50,
             shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20.0),
+              borderRadius: BorderRadius.circular(40.0),
             ),
             child: 
             Container(
@@ -35,13 +37,108 @@ class HomeScreen extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   const CircleAvatar(
-                    radius: 50,
+                    radius: 70,
                     backgroundColor: Colors.red,
                     foregroundImage: AssetImage(
                       "assets/images/myself.png",
                     ),
                   ),
                   const SizedBox(height: 15),
+                  Text(
+                    "Russel Garcia",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    "Web Developer",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  Text(
+                    "A kiddo who uses Bootstrap and Laravel in Web development. Currently playing around with design via Figma",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  TextButton(onPressed: (){}, 
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                      ),
+                      padding: MaterialStateProperty.all(EdgeInsets.symmetric(horizontal: 20, vertical: 10)),
+                      backgroundColor: MaterialStateProperty.all(Color.fromARGB(255, 113, 91, 182)),
+                      foregroundColor: MaterialStateProperty.all(Colors.white),
+                      overlayColor: MaterialStateProperty.all(Colors.grey),
+                      textStyle: MaterialStateProperty.all(
+                        TextStyle(
+                          fontSize: 15, 
+                          color: Colors.white,
+                          )
+                        ),
+                    ),
+                    child: Text("aragarcia@addu.edu.ph"),
+                  ),
+                  const SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(onPressed: (){}, 
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      icon: FaIcon(
+                        FontAwesomeIcons.facebook,
+                        color: Colors.grey,
+                        )
+                      ),
+                      IconButton(onPressed: (){}, 
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      icon: FaIcon(
+                        FontAwesomeIcons.instagram,
+                        color: Colors.grey,
+                        )
+                      ),
+                      IconButton(onPressed: (){}, 
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      icon: FaIcon(
+                        FontAwesomeIcons.twitter,
+                        color: Colors.grey,
+                        )
+                      ),
+                      IconButton(onPressed: (){}, 
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      icon: FaIcon(
+                        FontAwesomeIcons.discord,
+                        color: Colors.grey,
+                        )
+                      ),
+                      IconButton(onPressed: (){}, 
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      icon: FaIcon(
+                        FontAwesomeIcons.linkedin,
+                        color: Colors.grey,
+                        )
+                      ),
+                      IconButton(onPressed: (){}, 
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      icon: FaIcon(
+                        FontAwesomeIcons.dev,
+                        color: Colors.grey,
+                        )
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
