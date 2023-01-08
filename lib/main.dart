49be1fb5510346c8c13357a7711dac3cc64d0715
Giniatsu <aragarcia@addu.edu.ph>
2023-01-08 16:59:05 +0800

    starting second activity

--- lib/main.dart
+++ lib/main.dart
@@ -15,227 +15,39 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Size dsize = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Hello World'),
-      ),
-      body: /* MediaQuery.of(context).orientation == Orientation.portrait ? */
-      Container(
-        // color: Colors.red,
-        height: MediaQuery.of(context).size.height,
+      body: Container(
+        color: Color.fromARGB(255, 212, 193, 248),
         width: MediaQuery.of(context).size.width,
-        child: 
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text('Sign in to Continue', 
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.grey[500],
-                ),
-              ),
-              /* Text('Sign in to Continue', 
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.grey[500],
-                ),
-              ), */
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                //padding: EdgeInsets.only(top: 20),
-                width: MediaQuery.of(context).size.width * .40,
-                child: 
-                  TextField(
-                    obscureText: false,
-                    maxLines: 1,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Username',
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                //padding: EdgeInsets.only(top: 20),
-                width: MediaQuery.of(context).size.width * .40,
-                child: 
-                  TextField(
-                    obscureText: true,
-                    maxLines: 1,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width * .40,
-                color: Colors.blue,
-                child: 
-                  TextButton(
-                    onPressed: (){
-
-                  }, 
-                  child: 
-                    Text('Sign In', 
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width * .40,
-                child: Row(
+        child: Center(
+          child: Card(
+            elevation: 50,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: 
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              width: MediaQuery.of(context).size.width * .5,
+              height: 500,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Container(
-                    height: 55,
-                    width: 55,
-                    color: Colors.grey[300],
-                  ),
-                  Expanded(
-                    child: TextField(
-                    obscureText: false,
-                    maxLines: 1,
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                      border: InputBorder.none,
-                      labelText: "What's Happening",
-                      ),
+                  const CircleAvatar(
+                    radius: 50,
+                    backgroundColor: Colors.red,
+                    foregroundImage: AssetImage(
+                      "assets/images/myself.png",
                     ),
                   ),
+                  const SizedBox(height: 15),
                 ],
               ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width * .40,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(height: 40, width: 40, color: Colors.blue,),
-                        SizedBox(width: 5,),
-                        Container(height: 40, width: 40, color: Colors.red,),
-                        SizedBox(width: 5,),
-                        Container(height: 40, width: 40, color: Colors.green,),
-                        SizedBox(width: 5,),
-                        Container(height: 40, width: 40, color: Colors.yellow,),
-                        SizedBox(width: 5,),
-                        Container(height: 40, width: 40, color: Colors.orange,),
-                      ],
-                    ),
-                    TextButton(onPressed: (){
-                      }, 
-                      style: ButtonStyle(
-                        shape: MaterialStateProperty.all(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                        )
-                      ),
-                      padding: MaterialStateProperty.all(EdgeInsets.symmetric(horizontal: 25, vertical: 15)),
-                      backgroundColor: MaterialStateProperty.all(Colors.blue),
-                      foregroundColor: MaterialStateProperty.all(Colors.white),
-                      overlayColor: MaterialStateProperty.all(Colors.red[300]),
-                      textStyle: MaterialStateProperty.all(
-                        TextStyle(
-                          fontSize: 20,
-                          color: Colors.white,
-                        )
-                      ),
-                      ),
-                      child: Text('Tweet', 
-                        
-                      ),
-                    ),
-                  ],
-                  ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                padding: EdgeInsets.all(15),
-                height: 300,
-                width: dsize.width,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    containerNumber("1", Colors.red),
-                    containerNumber("2", Colors.green),
-                    containerNumber("3", Colors.blue),
-                    containerNumber("4", Colors.yellow),
-                    containerNumber("5", Colors.orange),
-                    containerNumber("6", Colors.pink),
-                    containerNumber("7", Colors.brown),
-                    containerNumber("8", Colors.cyan),
-                  ],
-                ),
-              )
-              /* Row(
-                children: [
-                  containerNumber(context, "X", Colors.blue),
-                  containerNumber(context, "A", Colors.green),
-                  containerNumber(context, "3", Colors.red),
-                  containerNumber(context, "4", Colors.yellow),
-                ],
-              ),
-              SizedBox(
-                height: 20,
-                ),
-              Wrap(
-                direction: Axis.horizontal,
-                children: [
-                  containerNumber(context, "1", Colors.blue),
-                  containerNumber(context, "2", Colors.green),
-                  containerNumber(context, "3", Colors.red),
-                  containerNumber(context, "4", Colors.yellow),
-                ],
-              ) */
-            ],
-          )
-      ) /* :
-      Container(
-        color: Colors.blue,
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width * .5,
-      ), */
+            ),
+          ),
+        ),
+      )
     );
-  }
-
-  Container containerNumber(String value, Color dcolor) {
-    return Container(
-                  height: 250,
-                  width: 500,
-                  color: dcolor,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(value,
-                        style: TextStyle(
-                          fontSize: 50,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                );
   }
 }
